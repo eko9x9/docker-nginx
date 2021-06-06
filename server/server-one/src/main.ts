241ead3ddcd2fadb383fd93af6c7e336ec6caf47
eko969 <ekonurrohman969@gmail.com>
2021-06-06 11:30:39 +0700

    create config for reverse proxy

--- server/server-one/src/main.ts
+++ server/server-one/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('x-api');
   await app.listen(2000);
 }
 bootstrap();
